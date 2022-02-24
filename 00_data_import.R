0a043877f1fa1c20caa5c6fa1933461b187e6bf6
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-24 13:28:28 +0100

    Salvini true id, last one was public id + details of 50 newest videos

--- 00_data_import.R
+++ 00_data_import.R
@@ -1,10 +1,10 @@
 library(tuber)
-yt_oauth(id, key)
+yt_oauth(id, key, token = '')
 
 #Start with Salvini because his YT account is up to date and tidy. He has many high quality video
 
-salvini_id <- list_channel_resources(filter = c(username = "MatteoSalviniOfficial"), part = "id")[[2]]
-#The function always gives the id in the second element of the list
+salvini_id <- "UCDjM54fZ-cD7F8uom767OhA"
+#Scraped from source, line 21. No way to obtain it from API
 
-salvini <- list_channel_resources(filter = c(channel_id = salvini_id), part = "contentDetails")
-
+salvini <- list_channel_videos(channel_id = salvini_id)
+#I have the id for the 50 newest videos + date and hour of publication
