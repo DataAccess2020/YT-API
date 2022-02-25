3a097608d33cfcbd919433ca9269b27b324e67d8
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-25 16:42:52 +0100

    Tuber's get_captions doesn't work anymore; use package youtubecaption instead - requires URLs not IDs

--- 01_data_preparation.R
+++ 01_data_preparation.R
@@ -13,20 +13,3 @@ salvini <- salvini %>%
   dplyr::rename("video_id" = "contentDetails.videoId")
 
 sal2 <- salvini
-
-
-
-library(tuber)
-yt_oauth(id, key, token = '')
-
-
-#I can't use get_all_channel_details because I would send too many requests in one time
-for (i in sal2$video_id) {
-  cat(i, " ")
-  stats <- get_video_details(i)
-  
-  yt_oauth(id, key, token = '')  #After almost 2000 IDs the loop stopped because of the token, so
-                                          #I'll try adding it in
-  Sys.sleep(1)
-}
-
