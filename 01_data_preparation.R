0019ff9c31b566d8c3c71b0e183a14afeb26aaea
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-25 13:24:16 +0100

    Get video details for every video

--- 01_data_preparation.R
+++ 01_data_preparation.R
@@ -2,6 +2,8 @@ library(rio)
 
 salvini <- import(file = "salvini.csv")
 
+sal_id <- import(file = "id.csv")
+
 salvini[1,]
 #ID and date column names are too long to type
 
@@ -9,3 +11,18 @@ salvini[1,]
 salvini <- salvini %>%
   dplyr::rename("date" = "contentDetails.videoPublishedAt") %>%
   dplyr::rename("video_id" = "contentDetails.videoId")
+
+sal2 <- salvini
+
+
+
+library(tuber)
+yt_oauth(id, key, token = '')
+
+
+for (i in sal2$video_id) {
+  cat(i, " ")
+  stats <- get_video_details(i)
+  
+  Sys.sleep(2)
+}
