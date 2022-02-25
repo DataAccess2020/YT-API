f2c143690e68f4c073a6f2c71fb1f1a4a3dcfe31
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-25 14:28:41 +0100

    Second tentative, add token in for loop

--- 01_data_preparation.R
+++ 01_data_preparation.R
@@ -20,9 +20,13 @@ library(tuber)
 yt_oauth(id, key, token = '')
 
 
+#I can't use get_all_channel_details because I would send too many requests in one time
 for (i in sal2$video_id) {
   cat(i, " ")
   stats <- get_video_details(i)
   
-  Sys.sleep(2)
+  yt_oauth(id, key, token = '')  #After almost 2000 IDs the loop stopped because of the token, so
+                                          #I'll try adding it in
+  Sys.sleep(1)
 }
+
