920b4ae3734915f4fac7d1fb013f2dc65cec1ed1
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-27 12:19:38 +0100

    Dataset with urls

--- 01_data_preparation.R
+++ 01_data_preparation.R
@@ -1,15 +1,21 @@
 library(rio)
 
-salvini <- import(file = "salvini.csv")
+#salvini <- import(file = "salvini.csv")
 
-sal_id <- import(file = "id.csv")
+#sal_id <- import(file = "id.csv")
 
-salvini[1,]
+#salvini[1,]
 #ID and date column names are too long to type
 
 
-salvini <- salvini %>%
-  dplyr::rename("date" = "contentDetails.videoPublishedAt") %>%
-  dplyr::rename("video_id" = "contentDetails.videoId")
+#salvini <- salvini %>%
+  #dplyr::rename("date" = "contentDetails.videoPublishedAt") %>%
+  #dplyr::rename("video_id" = "contentDetails.videoId")
 
-sal2 <- salvini
+
+
+#In 00_data_import I created a dataset called salvini_youtube because I discovered that I must use
+#Youtubecaptions' functions, which need URLs
+
+sal <- import("salvini_youtube.csv")
+
