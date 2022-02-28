bf2edbd5f1c06c19dbed6ed7880cf03de64116cd
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-28 20:03:01 +0100

    Subset list

--- 01_data_preparation.R
+++ 01_data_preparation.R
@@ -16,3 +16,20 @@ library(rio)
 
 #In 00_data_import I created a dataset called salvini_youtube because I discovered that I must use
 #Youtubecaptions' functions, which need URLs
+
+captions <- import("speech1.Rdata")
+
+
+divider <- function(x, n) split(x, cut(seq_along(x), n, labels = F))
+
+cap <- divider(captions, 4509)
+
+
+export(cap, "speech.Rdata")
+
+
+
+
+salvini <- import("salvini_youtube.csv")
+
+c <- data.frame(salvini$date, salvini$video_id, salvini$url) #cap[[1:4509]][["text"]])
