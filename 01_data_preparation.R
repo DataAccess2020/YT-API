7cce771958897b7dc35e80a2e4113275124dc3bd
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-01 17:25:12 +0100

    Fixed text issue over whole dataset

--- 01_data_preparation.R
+++ 01_data_preparation.R
@@ -19,17 +19,29 @@ library(rio)
 
 captions <- import("speech1.Rdata")
 
-
 divider <- function(x, n) split(x, cut(seq_along(x), n, labels = F))
 
 cap <- divider(captions, 4509)
-
 
 export(cap, "speech.Rdata")
 
 
 
-
 salvini <- import("salvini_youtube.csv")
 
-c <- data.frame(salvini$date, salvini$video_id, salvini$url) #cap[[1:4509]][["text"]])
+library(stringr)
+#test <- str_c(test[1:757], collapse = " ") this is the strategy for each text
+
+cap[["1"]][["text"]] <- str_c(cap[["1"]][["text"]], collapse = " ")
+#This works for the text for the first element of the list, so I get that I just need to change the "1"
+
+
+for (i in 1:4509) {
+  cap[[i]][["text"]] <- str_c(cap[[i]][["text"]], collapse = " ")
+}
+
+
+
+
+#c <- data.frame(salvini$date, salvini$video_id, salvini$url)
+
