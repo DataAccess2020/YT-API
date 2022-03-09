3d5f010bf0ec60e5f10d82ecebef9c4ba597e097
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-09 09:13:34 +0100

    Dataset

--- Scripts/00_data_import.R
+++ Scripts/00_data_import.R
@@ -1,158 +1,118 @@
 library(tuber)
 yt_oauth(id, key, token = '')
 
-#Start with Salvini because his YT account is up to date and tidy. He has many high quality video
+#I scraped the channel IDs from page source
 
-salvini_id
-#Scraped from source, line 21
+pd_ch <- list_channel_videos(channel_id = pd, max_results = 51)
+#max_results = 51 because anything >50 gives all the video in the account
 
-salvini <- list_channel_videos(channel_id = salvini_id, max_results = 51)
-#I have the id for every video + date and hour of publication. 4509 videos
-#I typed 51 because anything >50 gives you every video on channel
+m5s_ch <- list_channel_videos(channel_id = m5s, max_results = 51)
+
+fdi_ch <- list_channel_videos(channel_id = fdi, max_results = 51)
+
+lg_ch <- list_channel_videos(channel_id = lg, max_results = 51)
+
+fi_ch <- list_channel_videos(channel_id = fi, max_results = 51)
+
 
 
 library(rio)
-export(salvini, file = "salvini.csv")
 
-s_id <- data.frame(salvini_id)
+export(pd_ch, file = "pd_channel.csv")
 
-export(s_id, "id.csv")
+export(m5s_ch, file = "m5s_channel.csv")
+
+export(fdi_ch, file = "fdi_channel.csv")
+
+export(lg_ch, file = "lg_channel.csv")
+
+export(fi_ch, file = "fi_channel.csv")
+
 
 
 #Since Tuber's get_captions doesn't work anymore, I'm gonna be using Youtubecaption's get_captions
 #But it requires URLs, which I haven't
 
-salvini <- import("salvini.csv")
+library(stringr)
 
-sal <- salvini
+pd_url <- str_c("https://www.youtube.com/watch?v=", pd_ch$contentDetails.videoId[1:1955])
 
+m5s_url <- str_c("https://www.youtube.com/watch?v=", m5s_ch$contentDetails.videoId[1:20000])
 
-url <- str_c("https://www.youtube.com/watch?v=", sal$video_id[1:4509])
+fdi_url <- str_c("https://www.youtube.com/watch?v=", fdi_ch$contentDetails.videoId[1:5524])
 
-salv_data <- data.frame(sal[2:5], url)
+lg_url <- str_c("https://www.youtube.com/watch?v=", lg_ch$contentDetails.videoId[1:9548])
 
-export(salv_data, "salvini_youtube.csv")
-
-
-
-
-#Huge download of captions of every video
-
-sal <- import("salvini_youtube.csv")
+fi_url <- str_c("https://www.youtube.com/watch?v=", fi_ch$contentDetails.videoId[1:60])
 
 
 
+pd_data <- data.frame(pd_ch[2:5], pd_url)
+
+m5s_data <- data.frame(m5s_ch[2:5], m5s_url)
+
+fdi_data <- data.frame(fdi_ch[2:5], fdi_url)
+
+lg_data <- data.frame(lg_ch[2:5], lg_url)
+
+fi_data <- data.frame(fi_ch[2:5], fi_url)
+
+
+
+export(pd_data, "pd_data.csv")
+
+export(m5s_data, "m5s_data.csv")
+
+export(fdi_data, "fdi_data.csv")
+
+export(lg_data, "lg_data.csv")
+
+export(fi_data, "fi_data.csv")
+
+
+
+#Download of captions of every video
 
 library(youtubecaption)
 
-speech <- vector()
+library(rio)
+pd_data <- import("pd_data.csv")
+
+
+pd_captions <- vector()
 v <- vector()
 
-for (i in sal$url[1:500]) {
+
+for (i in pd_data$pd_url[1:1955]) {
   tryCatch({
     v <- get_caption(i, "it")
   }, error = function(e){})
   
   print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.5)
+  pd_captions <- append(pd_captions, v)
+  Sys.sleep(0.3)
 }
 
 
+save(... = pd_captions, file = "pd_captions.Rdata")
 
-for (i in sal$url[501:1000]) {
+
+
+m5s_data <- import("m5s_data.csv")
+
+m5s_captions <- vector()
+v <- vector()
+
+for (i in m5s_data$m5s_url[1:5000]) {
   tryCatch({
     v <- get_caption(i, "it")
   }, error = function(e){})
   
   print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.8)
+  m5s_captions <- append(m5s_captions, v)
 }
 
 
-
-for (i in sal$url[1001:1500]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
+save(... = m5s_captions, file = "m5s_captions.Rdata")
 
 
-
-for (i in sal$url[1501:2000]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
-
-
-
-for (i in sal$url[2001:2500]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
-
-
-
-for (i in sal$url[2501:3000]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
-
-
-
-for (i in sal$url[3001:3500]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
-
-
-
-for (i in sal$url[3501:4000]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
-
-
-
-for (i in sal$url[4001:4509]) {
-  tryCatch({
-    v <- get_caption(i, "it")
-  }, error = function(e){})
-  
-  print(i)
-  speech <- append(speech, v)
-  Sys.sleep(0.4)
-}
-
-
-save(... = speech, file = "speech1.Rdata")
