815f113dec713fc97f167da9cb4bb65c663c2f38
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-13 11:41:55 +0100

    Pd videos tokenization

--- 02_tokenization.R
+++ 02_tokenization.R
@@ -1,24 +1,7 @@
+#PD captions tokenization
+
 library(rio)
 pd_dataset <- import("Party Datasets/pd_dataset.csv")
-
-
-library(tidyverse)
-pd_dataset <- add_column(pd_dataset, token = NA)
-
-#install.packages("tokenizers")
-library(tokenizers)
-
-#prova <- tokenize_words(data$text[1:4509]) had to check the outcome first
-
-#install.packages("stopwords")
-library(stopwords)
-
-for (i in seq_along(1:1955)) {
-  pd_dataset$token[i] <- tokenize_words(pd_dataset$text[i], stopwords = stopwords("it"))
-  
-}
-
-
 
 
 #install.packages("tidytext")
@@ -29,18 +12,12 @@ text <- data_frame(Text = pd_dataset$text)
 words <- text %>% 
   unnest_tokens(output = word, input = Text)
 
-stop <- get_stopwords(language = "it")
 
-words <- words %>%
-  anti_join(stop)
+stop <- get_stopwords(language = "it")
 
 wordcounts <- words %>% count(word, sort = TRUE)
 
 wordcounts <- wordcounts %>%
-  anti_join(itastopwords)
-
-
-library(TextWiller)
-prova <- sentiment(wordcounts$word[1])
+  anti_join(stop)
 
 
