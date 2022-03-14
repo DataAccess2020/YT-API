c0b495e5162eaf1d35bb1acac19fed9117e185d6
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-14 17:29:09 +0100

    PD token + clean + 1 plot

--- 02_tokenization.R
+++ 02_tokenization.R
@@ -6,6 +6,7 @@ pd_dataset <- import("Party Datasets/pd_dataset.csv")
 
 #install.packages("tidytext")
 library(tidytext)
+library(tidyverse)
 
 text <- data_frame(Text = pd_dataset$text)
 
@@ -15,16 +16,29 @@ words <- text %>%
 
 stop <- get_stopwords(language = "it")
 
-wordcounts <- words %>% count(word, sort = TRUE)
+wordcounts <- words %>%
+  count(word, sort = TRUE)
 
 wordcounts <- wordcounts %>%
   anti_join(stop)
 
 library(stringr)
-error <- str_extract(wordcounts$word, ".*ã")
+wordcounts$word <- str_remove_all(wordcounts$word, ".*ã")
 
-error <- as.data.frame(error)
+wordcounts <- wordcounts[-which(wordcounts$word == ""), ]
 
-wordcounts <- wordcounts %>%
-  anti_join(error)
+
+
+#1 try plot
+wordcounts %>% 
+  filter(n > 6000) %>% 
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) + 
+  geom_col() +
+  coord_flip() +
+  labs(x = "Word \n", y = "\n Count ", title = "Frequent Words \n") +
+  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold") +
+  theme(plot.title = element_text(hjust = 0.5), 
+        axis.title.x = element_text(face="bold", colour="darkblue", size = 12),
+        axis.title.y = element_text(face="bold", colour="darkblue", size = 12))
 
