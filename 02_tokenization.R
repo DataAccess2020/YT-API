2aa84d45bb58a834d225447bc9620b77403bca02
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-15 14:54:57 +0100

    PD token cleaning + export

--- 02_tokenization.R
+++ 02_tokenization.R
@@ -3,23 +3,18 @@
 library(rio)
 pd_dataset <- import("Party Datasets/pd_dataset.csv")
 
+library(tidyverse)
+
+sw_ita <- scan("ita_stopwords.txt", what="", sep="\n")
+#Import a list of italian stopwords
+
 
 #install.packages("tidytext")
 library(tidytext)
-library(tidyverse)
-
-library(TextWiller)
-text <- removeStopwords(pd_dataset$text)
-
-
-
-
-
 text <- data_frame(Text = pd_dataset$text)
 
 words <- text %>% 
   unnest_tokens(output = word, input = Text)
-
 
 stop <- get_stopwords(language = "it")
 
@@ -28,24 +23,18 @@ wordcounts <- words %>%
 
 wordcounts <- wordcounts %>%
   anti_join(stop)
+#I removed stopwords again using another library to be thorough
 
 library(stringr)
 wordcounts$word <- str_remove_all(wordcounts$word, ".*ã")
 
+library(tm)
+for(i in 1:nrow(wordcounts)) {
+  wordcounts$word[i] <- removeWords(wordcounts$word[i], sw_ita)
+  
+  print(i)
+}
+
 wordcounts <- wordcounts[-which(wordcounts$word == ""), ]
 
-
-
-#1 try plot
-wordcounts %>% 
-  filter(n > 6000) %>% 
-  mutate(word = reorder(word, n)) %>% 
-  ggplot(aes(word, n)) + 
-  geom_col() +
-  coord_flip() +
-  labs(x = "Word \n", y = "\n Count ", title = "Frequent Words \n") +
-  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold") +
-  theme(plot.title = element_text(hjust = 0.5), 
-        axis.title.x = element_text(face="bold", colour="darkblue", size = 12),
-        axis.title.y = element_text(face="bold", colour="darkblue", size = 12))
-
+save(wordcounts, file = "PD_words.Rdata")
