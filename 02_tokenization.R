beb99e0abb63954d690d04db649d35ac41417b00
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-15 15:37:26 +0100

    Fratelli d'Italia token cleaning + export

--- 02_tokenization.R
+++ 02_tokenization.R
@@ -38,3 +38,90 @@ for(i in 1:nrow(wordcounts)) {
 wordcounts <- wordcounts[-which(wordcounts$word == ""), ]
 
 save(wordcounts, file = "PD_words.Rdata")
+
+
+
+
+
+
+#Forza Italia tokenization
+library(rio)
+fi_dataset <- import("Party Datasets/fi_dataset.csv")
+
+library(tidyverse)
+
+sw_ita <- scan("ita_stopwords.txt", what="", sep="\n")
+
+library(tidytext)
+text <- data_frame(Text = fi_dataset$text)
+
+words <- text %>% 
+  unnest_tokens(output = word, input = Text)
+
+stop <- get_stopwords(language = "it")
+
+wordcounts <- words %>%
+  count(word, sort = TRUE)
+
+wordcounts <- wordcounts %>%
+  anti_join(stop)
+
+library(stringr)
+wordcounts$word <- str_remove_all(wordcounts$word, ".*ã")
+
+library(tm)
+for(i in 1:nrow(wordcounts)) {
+  wordcounts$word[i] <- removeWords(wordcounts$word[i], sw_ita)
+  
+  print(i)
+}
+
+wordcounts <- wordcounts[-which(wordcounts$word == ""), ]
+
+save(wordcounts, file = "FI_words.Rdata")
+
+
+
+
+
+
+
+#Fratelli d'Italia tokenization
+
+library(rio)
+fdi_dataset <- import("Party Datasets/fdi_dataset.csv")
+
+library(tidyverse)
+
+sw_ita <- scan("ita_stopwords.txt", what="", sep="\n")
+#Import a list of italian stopwords
+
+
+#install.packages("tidytext")
+library(tidytext)
+text <- data_frame(Text = fdi_dataset$text)
+
+words <- text %>% 
+  unnest_tokens(output = word, input = Text)
+
+stop <- get_stopwords(language = "it")
+
+wordcounts <- words %>%
+  count(word, sort = TRUE)
+
+wordcounts <- wordcounts %>%
+  anti_join(stop)
+
+library(stringr)
+wordcounts$word <- str_remove_all(wordcounts$word, ".*ã")
+
+library(tm)
+for(i in 1:nrow(wordcounts)) {
+  wordcounts$word[i] <- removeWords(wordcounts$word[i], sw_ita)
+  
+  print(i)
+}
+
+wordcounts <- wordcounts[-which(wordcounts$word == ""), ]
+
+save(wordcounts, file = "FDI_words.Rdata")
