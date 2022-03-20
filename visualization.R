07b09b319e9285637ee2db1900ca2ca2a6727f64
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-03-20 19:11:29 +0100

    Visualization update + plot

--- visualization.R
+++ visualization.R
@@ -1,8 +1,5 @@
 library(ggplot2)
-library(dplyr)
-library(plotly)
 library(viridis)
-library(hrbrthemes)
 
 library(rio)
 pd_words <- import("Words/PD_words.Rdata")
@@ -11,11 +8,6 @@ fdi_words <- import("Words/FDI_words.Rdata")
 fi_words <- import("Words/FI_words.Rdata")
 m5s_words <- import("Words/M5S_words.Rdata")
 
-pd_dataset <- import("Party Datasets/pd_dataset.csv")
-lg_dataset <- import("Party Datasets/lg_dataset.csv")
-fdi_dataset <- import("Party Datasets/fdi_dataset.csv")
-fi_dataset <- import("Party Datasets/fi_dataset.csv")
-m5s_dataset <- import("Party Datasets/m5s_dataset.csv")
-
-
-
+ggplot(data, aes(x = word, y = count, fill = party)) + 
+  geom_bar(stat = "identity") +
+  scale_fill_viridis(discrete = T)
